b063e9e94d05158a56c20cc3f1a33100281e685e
sai-Sweshika <saisweshikakodurupaka@gmail.com>
2024-11-08 22:35:15 +0530

    I have practiced the string methods

---
+++ js.js
@@ -0,0 +1,58 @@
+// 1.let str = "Hello World!";
+// Relace the substring “World!” with “My Dear Friend” ..
+// Write the program for that
+// let str="Hello World!"
+// let res=str.replace("World!","My Dear Friend")
+// console.log(res);
+// o/p:Hello My Dear Friend
+
+// 2.let a=”Hungry”;
+// Repeat the “Hungry” for 10 times
+// Write the program for that
+// let a="Hungry \n"
+// console.log(a.repeat(10));
+// o/p:Hungry 
+// Hungry 
+// Hungry 
+// Hungry 
+// Hungry 
+// Hungry 
+// Hungry 
+// Hungry 
+// Hungry 
+// Hungry 
+
+// 3.let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+// Find the last index of the string “fox”
+
+// let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+// console.log(sentence.lastIndexOf("fox"));
+// o/p:49
+
+// 4.let message = "Hello, world!";
+// Find the string starts with “hi”
+// Write code for that
+
+// let message = "Hello, world!";
+// let a=message.startsWith("hi")
+// console.log(a);
+// o/p:false
+
+// 5.let fileName = "document.pdf";
+// Find the string ends with “.pdf”
+// Write code for that
+// let fileName = "document.pdf";
+// console.log(fileName.endsWith(".pdf"));
+// o/p:true
+
+
+
+
+
+
+
+
+
+
+
+
